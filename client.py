29cc49447c05d68385ba32133c7216cb0fc7bb97
Agostoner02 <lucas.agostoni@gmail.com>
2021-01-27 11:10:42 +0100

    .

--- client.py
+++ client.py
@@ -1,19 +1,17 @@
-#!/usr/bin/env python3
-
 import socket
 
 SERVER_ADDRESS = '127.0.0.1'  # The server's hostname or IP address
-SERVER_PORT = 65433        # The port used by the server
-sock_service = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock_service.connect((SERVER_ADDRESS, SERVER_PORT))
+SERVER_PORT = 65432        # The port used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((SERVER_ADDRESS, SERVER_PORT))
 dati = input("Inserisci messaggio per il server: ")
 dati = dati.encode()
-# Invia i dati al server
-sock_service.send(dati)
-# Riceve i dati dal server
-dati = sock_service.recv(2048)
+# Send data to server
+s.send(dati)
+# Receive response from server
+dati = s.recv(2048)
 if dati:
-    # Converte i dati dal formato  byte a stringa
+    # Convert back to string for python3
     dati = dati.decode()
     print("Ho ricevuto dal server: ")
     print(dati + '\n')
