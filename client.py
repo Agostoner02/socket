b041042f352d9b2c993438d93947881f12bce82e
Agostoner02 <lucas.agostoni@gmail.com>
2021-02-03 13:26:08 +0100

    ..

--- client.py
+++ client.py
@@ -2,15 +2,7 @@
 
 import socket
 
-# TEST SU COME FUNZIONA LA COMUNICAZIONE TRA CLIENT E SERVER
-# input_string = 'Hello'
-# print(type(input_string))
-# input_bytes_encoded = input_string.encode()
-# print(type(input_bytes_encoded))
-# print(input_bytes_encoded)
-# output_string = input_bytes_encoded.decode()
-# print(type(output_string))
-# print(output_string)
+
 
 SERVER_ADDRESS = '127.0.0.1'
 SERVER_PORT = 22224
