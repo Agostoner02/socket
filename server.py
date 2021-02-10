5ec02d893e8a1438b147b6e3e4309bd2ccc46a13
Agostoner02 <71756272+Agostoner02@users.noreply.github.com>
2021-02-10 12:53:32 +0100

    Update server.py
    commit

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 import socket
 
+#lasciando il campo vuoto sarebbe la stessa cosa(localhost)
 SERVER_ADDRESS = '127.0.0.1'
-
+#numero di porta, deve essere >1024 perchè le altre sono riservate
 SERVER_PORT = 22224
 
+#la funzione avva_server crea un endpoint di ascolto(sock:list) dal quale accettare cinnessioni in entrata
+#la socket di ascolto viene passata alla funzione ricevi_comandi la quale accetta richieste di connessione
+#e per ognuno crea una socket per i dati(sock_service) da cui ricevere le richieste e inviare le risposte
 
 def riceviComandi(socket):
     while True:
